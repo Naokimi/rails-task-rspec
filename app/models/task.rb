28a6a7a8122cc54a710dcac9990e0a39898940ce
Paulo DAlberti <phdalberti@gmail.com>
2023-03-16 10:54:49 +0900

    Add instance method for testing purposes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   validates :title, :description, presence: true
+
+  def short_description
+    description.length > 50 ? description[0..50] : description
+  end
 end
